f2e52e711df7a21a6b70fbadc1d3ff4a28c54dd0
GUSTAVO TIETGEN <gustavo.tietgen01@fatec.sp.gov.br>
2020-11-21 11:54:35 -0300

    Versão inicial do projeto de estudo para API de registro e persistencia de usuário.

---
+++ Application/Application.API/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Application.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        //GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        //GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return id.ToString();
+        }
+
+        //POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        //PUT api/values
+        [HttpPut]
+        public void Put([FromBody]string value)
+        {
+        }
+    }
+}
