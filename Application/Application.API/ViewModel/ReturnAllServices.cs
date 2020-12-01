5c6fd16464c084655d26a42d520091702cf90dfa
GUSTAVO TIETGEN <gustavo.tietgen01@fatec.sp.gov.br>
2020-11-30 22:26:11 -0300

    API de usuarios concluida

---
+++ Application/Application.API/ViewModel/ReturnAllServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.API.ViewModel
+{
+    public class ReturnAllServices
+    {
+        public bool Result { get; set; }
+        public string Message { get; set; }
+
+    }
+}
